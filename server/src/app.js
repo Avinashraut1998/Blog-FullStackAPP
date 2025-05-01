fd7d8bffae20c2e5abb09f35878e8163b82037fb
adarshmalviya <adarsh@truecover.net>
2025-05-01 19:45:36 +0530

    server setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,25 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
 import express from 'express';
 import path from 'path';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
 
 app.get("/", (req, res) =>  res.json({ message: "App is Working" }))
 
 
 app.use((req, res) => {
-    console.log(import.meta.dirname)
     res.status(404).sendFile(path.join(import.meta.dirname, "views", "404.html"));
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/src/config/db.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import {mongoose} from "mongoose";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+
+export const connectDB = async () => {
+    try{
+        await mongoose.connect(DATABASE_URL);
+        console.log('MongoDB connected ✅');
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
